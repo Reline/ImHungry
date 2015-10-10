0b0ddbbc0885594b63107fa1634dbbb596b0b8c3
FunnyGopher <huntsman.kyle@gmail.com>
2015-10-10 03:48:22 -0700

    Added a change listener to the Slider. Added the slider to the ImHungryFragment.

--- app/src/main/java/com/github/funnygopher/imhungry/ImHungryFragment.java
+++ app/src/main/java/com/github/funnygopher/imhungry/ImHungryFragment.java
@@ -5,12 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ImHungryFragment extends Fragment {
 
+    private Slider mPriceSlider;
+    private TextView mPriceText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.fragment_im_hungry, container, false);
-        return v;
+        View view = inflater.inflate(R.layout.fragment_im_hungry, container, false);
+
+        mPriceSlider = (Slider) view.findViewById(R.id.slider_price);
+        mPriceText = (TextView) view.findViewById(R.id.tv_slider_price);
+        mPriceText.setText("$");
+
+        mPriceSlider.setOnSliderChangeListener(new Slider.OnSliderChangeListener() {
+            @Override
+            public void onSliderChange(Slider slider, int index, int prevIndex) {
+                String moneyString = "";
+                mPriceText.setTextSize(20);
+                for (int i = 0; i <= index; i += 2) {
+                    moneyString += '$';
+                }
+
+                for(int i = 1; i <= index; i += 2) {
+                    mPriceText.setTextSize(20 + (10 * i));
+                }
+
+                mPriceText.setText(moneyString);
+            }
+        });
+
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/github/funnygopher/imhungry/Slider.java
@@ -0,0 +1,98 @@
+package com.github.funnygopher.imhungry;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class Slider extends SeekBar {
+
+    private static int POINT_VALUE = 100;
+
+    private OnSliderChangeListener mListener;
+    private int mPoints;
+    private int index;
+
+    public Slider(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Retrieves attributes
+        TypedArray array = context.getTheme()
+                .obtainStyledAttributes(attrs, R.styleable.Slider, 0, 0);
+        try {
+            mPoints = array.getInteger(R.styleable.Slider_points, 2);
+
+            // The first point should always be at 0, so
+            // this allows the seek bar to be equally
+            // separated into n - 1 segments
+            setMax((mPoints - 1) * POINT_VALUE);
+        } finally {
+            if(array != null) {
+                array.recycle();
+            }
+        }
+
+        init();
+    }
+
+    private void init() {
+        index = 0;
+
+        this.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Finds the closes point, and then snaps the bar to it
+                int smallestABSIndex = 0;
+                float smallestABS = -1;
+                for (int i = 0; i < mPoints; i++) {
+                    int abs = Math.abs(i * POINT_VALUE - progress);
+                    if (smallestABS == -1) {
+                        smallestABS = abs;
+                    }
+
+                    if (abs < smallestABS) {
+                        smallestABS = abs;
+                        smallestABSIndex = i;
+                    }
+                }
+                int prevIndex = index;
+                index = smallestABSIndex;
+
+                if(mListener != null) {
+                    mListener.onSliderChange(Slider.this, index, prevIndex);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                seekBar.setProgress(index * POINT_VALUE);
+            }
+        });
+    }
+
+    public void setPoints(int points) {
+        mPoints = points;
+        setMax((mPoints - 1) * POINT_VALUE);
+    }
+
+    public void setIndex(int index) {
+        setProgress(index * POINT_VALUE);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setOnSliderChangeListener(Slider.OnSliderChangeListener listener) {
+        mListener = listener;
+    }
+
+    public interface OnSliderChangeListener {
+        void onSliderChange(Slider slider, int index, int prevIndex);
+    }
+}
